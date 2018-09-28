d31e9ccd0fbbfda751fd19f92c875242d068c9e4
Luis Urán <luisuran.bit@gmail.com>
2018-09-28 17:42:35 -0300

    Agrego funcionalidad de búsqueda

--- src/Componentes/BarraBusqueda/BarraBusqueda.js
+++ src/Componentes/BarraBusqueda/BarraBusqueda.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react'
+import axios from 'axios';
 
 class BarraBusqueda extends Component {
-  render() {
+	constructor() {
+		super();
+		this.state = {
+			textoBusqueda: '',
+		}
+	}
+
+	onBusquedaChange = e => {
+		this.setState({
+			textoBusqueda: e.target.value
+		})
+	}
+
+	handleBusqueda = () => {
+		axios.get('https://www.googleapis.com/books/v1/volumes?q=intitle:' + this.state.textoBusqueda.split(' ').join('+'))
+    .then(response => console.log(response))
+	}
+
+	render() {
     return (
       <div>
-        <form class="card card-sm mt-2">
-          <div class="card-body row no-gutters align-items-center">
-              <div class="col">
-                  <input class="form-control form-control-lg form-control-borderless" type="search" placeholder="Busca tus libros favoritos"/>
+        <form className="card card-sm mt-2" onSubmit={this.handleBusqueda}>
+          <div className="card-body row no-gutters align-items-center">
+              <div className="col">
+									<input className="form-control form-control-lg form-control-borderless" type="search" placeholder="Busca tus libros favoritos" onChange={this.onBusquedaChange}/>
               </div>
-              <div class="col-auto">
-                  <button class="btn btn-lg btn-success" type="submit">Buscar</button>
+              <div className="col-auto">
+                  <button className="btn btn-lg btn-success">Buscar</button>
               </div>
           </div>
         </form>
