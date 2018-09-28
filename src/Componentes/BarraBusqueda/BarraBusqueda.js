0099c8c061caabdd56c5f8424d06bc341b966f58
Luis Urán <luisuran.bit@gmail.com>
2018-09-28 15:40:05 -0300

    Agrego barra de búsquedas

---
+++ src/Componentes/BarraBusqueda/BarraBusqueda.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+class BarraBusqueda extends Component {
+  render() {
+    return (
+      <div>
+        <form class="card card-sm">
+          <div class="card-body row no-gutters align-items-center">
+              <div class="col">
+                  <input class="form-control form-control-lg form-control-borderless" type="search" placeholder="Busca tus libros favoritos"/>
+              </div>
+              <div class="col-auto">
+                  <button class="btn btn-lg btn-success" type="submit">Buscar</button>
+              </div>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default BarraBusqueda;
