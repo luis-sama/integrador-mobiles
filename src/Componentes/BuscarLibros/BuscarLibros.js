21e6ed7badcdf6e5e81d76d6333d0cb465f5dbbf
Luis Urán <luisuran.bit@gmail.com>
2018-09-30 11:44:50 -0300

    Arreglo error libros no encontrados

--- src/Componentes/BuscarLibros/BuscarLibros.js
+++ src/Componentes/BuscarLibros/BuscarLibros.js
@@ -12,21 +12,26 @@ class BuscarLibros extends Component {
           const { libros } = value
           let listaLibros = null
           if (libros) {
-            listaLibros = libros.data.items.map(libro => {
-              return (
-                <div className="col-md-2 m-1">
-                <Libro
-                  imagen={libro.volumeInfo.imageLinks.thumbnail}
-                  descripcion={libro.volumeInfo.description}
-                  titulo={libro.volumeInfo.title}
-                  autor={libro.volumeInfo.authors}
-                  editorial={libro.volumeInfo.publisher}
-                  id={libro.id}
-                  key={libro.id}
-                />
-                </div>
-              )
-            })
+            if (libros.data.totalItems !== 0) {
+              listaLibros = libros.data.items.map(libro => {
+                return (
+                  <div className="col-md-2 m-1">
+                  <Libro
+                    imagen={libro.volumeInfo.imageLinks.thumbnail}
+                    descripcion={libro.volumeInfo.description}
+                    titulo={libro.volumeInfo.title}
+                    autor={libro.volumeInfo.authors}
+                    editorial={libro.volumeInfo.publisher}
+                    id={libro.id}
+                    key={libro.id}
+                  />
+                  </div>
+                )
+              })
+            } else {
+              listaLibros = <h1>No se encontraron coincidencias</h1>
+            }
+              
           }
           return (
             <div>
