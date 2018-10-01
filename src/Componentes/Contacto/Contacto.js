130fc9da4f0ac3c9d8ad92c66b886e27dbfdb117
Luis Urán <luisuran.bit@gmail.com>
2018-10-01 00:05:41 -0300

    Agrego el mapita

--- src/Componentes/Contacto/Contacto.js
+++ src/Componentes/Contacto/Contacto.js
@@ -6,6 +6,7 @@ const Contacto = () => {
     <h1>SamaProject Enterprise</h1><br/><br/>
     <h3>(011) - 0303456</h3>
     <h3>samaprojectenterprise@gmail.com</h3><br/>
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3271.347359294333!2d-57.958444185041365!3d-34.92282438195855!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a2e62dd769b9d7%3A0x8e5a62b28764b5ef!2sCatedral+de+La+Plata!5e0!3m2!1ses-419!2sar!4v1538362951004" width="600" height="450" frameborder="0" style={{border:0}} allowfullscreen></iframe>
   </div>
   )
 }
